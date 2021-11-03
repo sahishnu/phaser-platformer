35f6dadceba637e601ed994c5a1d127b5706029f
Sash Patel <bhoolku5443@gmail.com>
2021-11-03 18:42:18 -0400

    Init project

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+var players = {};
+app.use(express.static(__dirname + "/public"));
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: Math.floor(Math.random() * 2) == 0 ? "red" : "blue",
+  };
+  // send the players object to the new player
+  socket.emit("currentPlayers", players);
+  // update all other players of the new player
+  socket.broadcast.emit("newPlayer", players[socket.id]);
+  // when a player disconnects, remove them from our players object
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit("playerDisconnect", socket.id);
+  });
+
+  // when a player moves, update the player data
+  socket.on("playerMovement", function (movementData) {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    players[socket.id].flipX = movementData.flipX;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit("playerMoved", players[socket.id]);
+  });
+});
+server.listen(8081, function () {
+  console.log(`Listening on ${server.address().port}`);
+});
